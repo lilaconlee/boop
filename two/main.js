0305242b1333b0ac1d2479e45fcf6694dde987c9
lilaconlee <lila.conlee@gmail.com>
2016-02-26 20:56:22 -0600

    Add so many flakes

--- two/main.js
+++ two/main.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var fps = 40;
+var fps = 500;
 var h = window.innerHeight * window.devicePixelRatio;
 var w = window.innerWidth * window.devicePixelRatio;
-var flake = [50,0];
+var flakes = [];
 var base = '#1A2150';
 var alph = [1,.2,.1,.05];
 
@@ -12,17 +12,30 @@ function background() {
   ctx.fillRect(0,0,w,h);
 }
 
-function draw() {
-  for (var i = 0; i <= 3; i++) {
-    var x = 10 * i;
-
-    ctx.fillStyle = 'rgba(255, 255, 255, ' + alph[i] + ')';
-    ctx.fillRect(flake[0],flake[1] - x,10,10);
-
-    ctx.fillStyle = base;
-    ctx.fillRect(flake[0],flake[1] - (x + 10),10,10);
+function makeFlakes() {
+  for (var i = 0; i <= w/30; i++) {
+    flakes.push([Math.random() * w, Math.random() * h]);
   }
-  flake[1]++;
+}
+
+function draw() {
+  flakes.forEach(function (flake) {
+    for (var i = 0; i <= 3; i++) {
+      var x = 10 * i;
+
+      ctx.fillStyle = 'rgba(255, 255, 255, ' + alph[i] + ')';
+      ctx.fillRect(flake[0],flake[1] - x,10,10);
+
+      ctx.fillStyle = base;
+      ctx.fillRect(flake[0]-1,flake[1] - (x + 10),12,10);
+    }
+    if (flake[1] >= h + 30) {
+      flake[0] = Math.random() * w;
+      flake[1] = 0;
+    } else {
+      flake[1]++;
+    }
+  });
 
   setTimeout(function() {
       requestAnimationFrame(draw);
@@ -38,5 +51,6 @@ function fillScreen() {
 }
 
 fillScreen();
+makeFlakes();
 background();
 draw();
