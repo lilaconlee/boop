6e2f1693efb71b5251f3cd2e8d28300097110051
lilaconlee <lila.conlee@gmail.com>
2016-03-20 10:26:08 -0500

    make less terrible

--- four/main.js
+++ four/main.js
@@ -1,17 +1,18 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var fps = 2;
+var fps = 5000;
 var h = window.innerHeight * window.devicePixelRatio;
 var w = window.innerWidth * window.devicePixelRatio;
 var l = w/5;
 var base = '#2a334f';
 var colors;
+var cur = [];
+var next = [];
 
 function background() {
   ctx.fillStyle = base;
   ctx.fillRect(0,0,w,h);
 }
-
 
 function getColors() {
   var color = Please.make_color({
@@ -29,20 +30,34 @@ function draw() {
   background();
 
   for (var i = 0; i < 5; i++) {
-    var rand = Math.random() * 1000;
+
+    if (cur[i] < next[i]) {
+      cur[i] = cur[i] + 1;
+    } else if (cur[i] > next[i]) {
+      cur[i] = cur[i] - 1;
+    } else if (cur[i] === next[i]) {
+      next[i] = Math.floor(Math.random() * 1000);
+    }
 
     ctx.beginPath();
     ctx.moveTo(i * l, h);
-    ctx.quadraticCurveTo(((i*2) + 1) * (l/2), rand,(i+1) * l, h);
+    ctx.quadraticCurveTo(((i*2) + 1) * (l/2), cur[i],(i+1) * l, h);
     ctx.closePath();
     ctx.fillStyle = ctx.strokeStyle = colors[i];
     ctx.fill();
     ctx.stroke();
 
-    rand = Math.random() * (1000 - 200) + 200;
+    if (cur[i+5] < next[i+5]) {
+      cur[i+5] = cur[i+5] + 1;
+    } else if (cur[i+5] > next[i+5]) {
+      cur[i+5] = cur[i+5] - 1;
+    } else if (cur[i+5] === next[i+5]) {
+      next[i+5] = Math.floor(Math.random() * (1000 - 200) + 200);
+    }
+
     ctx.beginPath();
     ctx.moveTo(i * l, 0);
-    ctx.quadraticCurveTo(((i*2) + 1) * (l/2), rand,(i+1) * l, 0);
+    ctx.quadraticCurveTo(((i*2) + 1) * (l/2), cur[i+5],(i+1) * l, 0);
     ctx.closePath();
     ctx.fillStyle = ctx.strokeStyle = colors[i];
     ctx.fill();
@@ -52,6 +67,18 @@ function draw() {
   setTimeout(function() {
     requestAnimationFrame(draw);
   }, 1000/fps);
+}
+
+function init() {
+  for (var i = 0; i < 5; i++) {
+    cur.push(Math.floor(Math.random() * 1000));
+    next.push(Math.floor(Math.random() * 1000));
+  }
+
+  for (var i = 0; i < 5; i++) {
+    cur.push(Math.floor(Math.random() * (1000 - 200) + 200));
+    next.push(Math.floor(Math.random() * (1000 - 200) + 200));
+  }
 }
 
 function fillScreen() {
@@ -65,4 +92,5 @@ function fillScreen() {
 getColors();
 fillScreen();
 background();
+init();
 draw();
